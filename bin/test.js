d21f276fd95da98a4bdf78adce9a1dd700dca1f5
Feross Aboukhadijeh <feross@feross.org>
2016-07-12 13:09:18 -0700

    add sanity test that repos exist on GitHub

---
+++ bin/test.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+/**
+ * Sanity check that all repos in test.json actually exist on GitHub
+ */
+
+var get = require('simple-get')
+var packages = require('../')
+var series = require('run-series')
+
+series(packages.test.map(function (pkg) {
+  return function (cb) {
+    console.log('checking ' + pkg.repo + '...')
+    get(pkg.repo, function (err, res) {
+      if (res.statusCode !== 200) {
+        err = new Error('Non-200 status code ' + res.statusCode)
+      }
+      if (err) {
+        err.message = pkg.name + ': ' + err.message
+        cb(err)
+      } else {
+        cb(null)
+      }
+    })
+  }
+}), function (err) {
+  if (err) throw err
+})
